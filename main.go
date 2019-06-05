ab482bbcaff2dcc2a84e302fb3a9dd33b8fe0bbe
Jaime Melis <jmelis@redhat.com>
2019-06-05 13:28:31 +0200

    main

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"os"
 
 	"github.com/jmelis/catalog-image-go/pkg/catalog"
@@ -14,8 +13,7 @@ var token = os.Getenv("GITHUB_TOKEN")
 var gitName = "Jaime Melis"
 var gitEmail = "j.melis@gmail.com"
 var gitBranch = "master"
-
-// var gitDir = "/home/jmelis/borrar/tmpgit"
+var gitDir = "/home/jmelis/borrar/tmpgit"
 var bundlePath = "/home/jmelis/work/git/catalog-image/test/fixtures/bundles/0.1.700-0000000"
 
 func checkIfError(err error) {
@@ -33,7 +31,7 @@ func main() {
 		GitName:   gitName,
 		GitEmail:  gitEmail,
 		GitBranch: gitBranch,
-		// GitDir:    gitDir,
+		GitDir:    gitDir,
 	}
 
 	store, err := catalog.NewGitStore(gitStoreOptions)
@@ -42,20 +40,10 @@ func main() {
 	c := catalog.NewCatalog(operator, store)
 
 	c.Load()
-	for _, b := range c.Bundles {
-		fmt.Println(b.CSV.Version())
-	}
-	fmt.Println("hi")
 	c.AddBundle(bundlePath)
-	for _, b := range c.Bundles {
-		fmt.Println(b.CSV.Version())
-	}
+	c.Save()
 
-	// c.Save()
-
-	// err = c.WriteFile()
-	// checkIfError(err)
-
-	// err = c.Save()
-	// checkIfError(err)
+	// for _, b := range c.Bundles {
+	// 	fmt.Println(b.CSV.Version())
+	// }
 }
